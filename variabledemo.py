1fa5b07987791a4dce752f161069ab4333553a78
SakivV <banagevikas@gmail.com>
2017-10-04 16:20:52 +0200

    Add files via upload

---
+++ variabledemo.py
@@ -0,0 +1,31 @@
+
+### String Demo ###
+
+fname = "viKaS"
+
+print(fname.title())
+
+#UPPAER Case
+firstname = fname.upper()
+
+#Combine
+lname = "banGe"
+lastname = lname.upper()
+print(firstname+ " " + lastname)
+
+
+### Number Demo ###
+interest = 7.6              #Float
+loantype = 'Personal Loan'  #String
+amount = 400000             #Integer
+
+print("Loan Amount\t" , amount)
+print("Your " + loantype + ":" + str(amount)+ " with interest rate" + str(interest) + " is approved.") #Explicit Conversion
+
+## Muliple assingment
+a,b,c = 1,2,"Ready"
+
+
+
+
+
